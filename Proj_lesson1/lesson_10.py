b439a56ab2cedee7e22a2bec8c69122533f032e0
irynayaroshenko <kosa.kolosok@gmail.com>
2023-04-20 08:40:10 +0300

    proj lessons files

---
+++ Proj_lesson1/lesson_10.py
@@ -0,0 +1,15 @@
+"""Modules"""
+# if __name__ == '__main__'
+from test_modules.hello import a
+print(a)
+
+# 3 types of modules: built-in (math, random, etc) should be imported first; installed by pip (requests, numpy,etc)
+# should be imported second; modules written by me should be imported last (first from other folder, second - from the
+# same folder where my current file where I need to import module)
+
+
+# python, sql, rest, web framework, docker, cron (for data update), git (mercury), ORM libraries
+# python development road map  https://brainstation.io/career-guides/what-does-a-python-developer-do
+# 3-4 pet projects
+# graphic work:  use of kinter, qt, kivi
+# AWS, nginx
